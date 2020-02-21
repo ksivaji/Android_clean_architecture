7be42c388d3e97656972700eee5555595326a97e
Siva <koya.sivaji@gmail.com>
2020-02-21 14:33:18 -0500

    Updated Room and Dagger library versions

--- app/src/main/java/com/example/githubrepos/App.kt
+++ app/src/main/java/com/example/githubrepos/App.kt
@@ -1,17 +1,17 @@
 package com.example.githubrepos
 
-import android.app.Activity
 import android.app.Application
 import com.example.githubrepos.di.DaggerAppComponent
 import com.facebook.stetho.Stetho
+import dagger.android.AndroidInjector
 import dagger.android.DispatchingAndroidInjector
-import dagger.android.HasActivityInjector
+import dagger.android.HasAndroidInjector
 import timber.log.Timber
 import javax.inject.Inject
 
-class App : Application(), HasActivityInjector {
+class App : Application(), HasAndroidInjector {
     @Inject
-    lateinit var dispatchingAndroidInjector: DispatchingAndroidInjector<Activity>
+    lateinit var dispatchingAndroidInjector: DispatchingAndroidInjector<Any>
 
     override fun onCreate() {
         super.onCreate()
@@ -26,5 +26,6 @@ class App : Application(), HasActivityInjector {
             .inject(this)
     }
 
-    override fun activityInjector() = dispatchingAndroidInjector
+    override fun androidInjector(): AndroidInjector<Any> = dispatchingAndroidInjector
+
 }
